04c5c690589b59fd6245edd819d90f0b21eb230c
kiburei <akiburei@gmail.com>
2017-05-19 15:09:34 +0300

    activate mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,18 +4,21 @@ class UserMailer < ApplicationMailer
 
   def user_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Thanks for signing up for our amazing app')
+    mail(to: @user.email, subject: 'Thanks for signing up to our amazing app')
   end
 
   def purchase(user_id)
     @user = User.find(user_id)
     mail(to: @user.email, subject: 'Interest to buy shares')
+    @admin = "member@invela.co.ke"
+    mail(to: @admin, subject: 'Interest to buy shares')
   end
 
   def sell(user_id)
     @user = User.find(user_id)
     mail(to: @user.email, subject: 'Interest to sell shares')
-
+    @admin = "member@invela.co.ke"
+    mail(to: @admin, subject: 'Interest to sell shares')
   end
 
 end
